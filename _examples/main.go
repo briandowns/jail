4b3d237d5c4ecf3352d308fee161f0dc3b4952e1
bdowns <brian.downs@gmail.com>
2018-04-30 11:29:44 -0700

    example update

--- _examples/main.go
+++ _examples/main.go
@@ -4,34 +4,29 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"time"
 
 	"github.com/briandowns/jail"
 )
 
 func main() {
-	jo := &jail.JailOpts{
+	o := &jail.Opts{
 		Path:     "/zroot/jails/build",
 		Name:     "jailname",
 		Hostname: "hostname",
 		IP4:      "192.168.0.200",
 		Chdir:    true,
 	}
-
-	jid, err := jail.Jail(jo)
+	jid, err := jail.Jail(o)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("JID: %d\n", jid)
-	time.Sleep(30 * time.Second)
-	fmt.Println("/ director listing in jail")
-	files, err := ioutil.ReadDir("./")
+	fmt.Printf("JID: %d - / director listing in jail", jid)
+	files, err := ioutil.ReadDir(".")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
 	for _, f := range files {
 		fmt.Println(f.Name())
 	}
